bd3209007d9a29acc931d4e0248fb75145fc667e
Lidija Jezova <lidija.jezova@gmail.com>
2023-02-04 00:15:52 +0200

    add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,40 @@
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+    const stylesLoader = {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+            // Creates `style` nodes from JS strings
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: /\.module\./,
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
+                    }
+                }
+            },
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
+
+    // If we don't use TS, then use babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [
+        typescriptLoader,
+        stylesLoader
+    ]
+}
---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,19 @@
+import htmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
+    return [
+        new htmlWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        // нативный css браузером обрабатывается параллельно и быстрее, чем код внутри js.
+        // А для дев сборки мы этим можем принебречь, так как у нас все в памяти хранится и файлы генерить нет смысла
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        })
+    ]
+}

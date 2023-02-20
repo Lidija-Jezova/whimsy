66611157843da0298317cce5295fd219e825fbe9
Lydia20 <lidija.jezova@gmail.com>
2023-02-20 12:46:10 +0200

    add PageLoader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,10 +3,17 @@ import type { RouteObject } from 'react-router-dom'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { routeConfig } from 'app/providers/router/config/routeConfig'
 import AppLayout from 'shared/ui/AppLayout/AppLayout'
+import { PageLoader } from 'widgets/PageLoader/PageLoader'
 
 const AppRouter = () => {
     const routerChildren: RouteObject[] = Object.values(routeConfig).map(({ element, path }) => {
-        return { element: element, path }
+        return { 
+            element:  
+            <Suspense fallback={<PageLoader/>}>
+                {element}
+            </Suspense>, 
+            path 
+        }
     })
 
     const router = createBrowserRouter([
@@ -18,9 +25,7 @@ const AppRouter = () => {
     ])
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <RouterProvider router={router} />
-        </Suspense>
+        <RouterProvider router={router} />
     )
 }
 
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Loader } from 'shared/ui/Loader/Loader'
 
 const MainPage = () => {
     const { t } = useTranslation('main')
